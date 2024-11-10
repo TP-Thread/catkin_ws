a132e1ea576bc68c06da33df2565fcf618745097
TP-Thread <TP-Thread@outlook.com>
2024-11-10 10:51:31 +0800

    修改

--- src/robot_vision/scripts/yolov5_detector.py
+++ src/robot_vision/scripts/yolov5_detector.py
@@ -8,13 +8,7 @@ from sensor_msgs.msg import Image
 from robot_vision.msg import BoundingBox, BoundingBoxes
 
 class ObjectDetector:
-    def __init__(self):
-        # 指定yolov5的源码路径，位于robot_vision/yolov5/
-        yolov5_path = rospy.get_param('~yolov5_path', '')
-        # 指定yolov5的权重文件路径
-        weight_path = rospy.get_param('~weight_path', '')
-        # yolov5的模型置信度阈值，置信度低于conf的预测结果会被忽略
-        conf = rospy.get_param('~conf', '0.8')
+    def __init__(self, yolov5_path, weight_path, conf, sub_image_topic):
         # 使用pytorch加载yolov5模型，torch.hub.load会从robot_vision/yolov5/中找名为hubconf.py的文件
         # hubconf.py文件包含了模型的加载代码，负责指定加载哪个模型
         self.model = torch.hub.load(yolov5_path, 'custom', path=weight_path, source='local')
@@ -22,7 +16,7 @@ class ObjectDetector:
 
         # 创建cv_bridge，声明图像的发布者和订阅者
         self.cv_bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
+        self.image_sub = rospy.Subscriber(sub_image_topic, Image, self.callback)
         self.image_pub = rospy.Publisher("/yolov5/detection_image", Image, queue_size=1)
         # 发布识别到的目标框信息，BoundingBoxes是自定义的消息类型
         self.target_pub = rospy.Publisher("/yolov5/detection_tags",  BoundingBoxes, queue_size=1) 
@@ -70,7 +64,17 @@ class ObjectDetector:
 def main():
     rospy.init_node("yolov5_detector")
     rospy.loginfo("starting yolov5_detector node")
-    ObjectDetector()
+
+    # 指定yolov5的源码路径，位于robot_vision/yolov5/
+    yolov5_path = rospy.get_param('~yolov5_path', '')
+    # 指定yolov5的权重文件路径
+    weight_path = rospy.get_param('~weight_path', '')
+    # yolov5的模型置信度阈值，置信度低于conf的预测结果会被忽略
+    conf = rospy.get_param('~conf', '0.8')
+    # 订阅的图像话题
+    sub_image_topic = rospy.get_param('~sub_image_topic', '')
+    
+    ObjectDetector(yolov5_path, weight_path, conf, sub_image_topic)
     rospy.spin()
     cv2.destroyAllWindows()
 
